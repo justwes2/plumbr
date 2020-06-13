d7aa4fdbeee6864578936e00df64d0010ac5581e
wes <wes.coffay@gmail.com>
2020-06-12 23:45:28 -0400

    added infra template

--- make.sh
+++ make.sh
@@ -2,4 +2,6 @@
 mkdir function infra nonprod prod
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PIPELINE=$(<$DIR/PIPELINE)
+STARTER=$(<$DIR/STARTER)
 echo "$PIPELINE">Jenkinsfile
+echo "$STARTER">infra/main.tf
