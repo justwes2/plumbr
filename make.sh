a8802e6e94004b11b4ece84d99d6664f186a21d4
wes <wes.coffay@gmail.com>
2020-06-12 23:51:25 -0400

    added np/pd modules

--- make.sh
+++ make.sh
@@ -3,5 +3,8 @@ mkdir function infra nonprod prod
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PIPELINE=$(<$DIR/PIPELINE)
 STARTER=$(<$DIR/STARTER)
+CONSTRUCTOR=$(<$DIR/CONSTRUCTOR)
 echo "$PIPELINE">Jenkinsfile
 echo "$STARTER">infra/main.tf
+echo "$CONSTRUCTOR">nonprod/main.tf
+echo "$CONSTRUCTOR">prod/main.tf
